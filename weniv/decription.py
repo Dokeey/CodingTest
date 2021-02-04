a0b15adec414ca873f333a46a6864aba7857890b
dokeey <ehdgnv@gmail.com>
2021-02-04 16:18:06 +0900

    ADD: Q5 tree 관련 문제 풀이

--- weniv/decription.py
+++ weniv/decription.py
@@ -16,39 +16,3 @@ str = ['   + -- + - + -   ',
        '   + - + - + - +   ']
 answer = ''.join([chr(int(s.replace(' ', '').replace('+', '1').replace('-', '0'), 2)) for s in str])
 print(answer)
-
-stone = [5, 3, 4, 1, 3, 8, 3]
-# stone = [1, 2, 1, 4]
-dogs = [{
-    "이름": "루비독",
-    "나이": "95년생",
-    "점프력": "3",
-    "몸무게": "4",
-}, {
-    "이름": "피치독",
-    "나이": "95년생",
-    "점프력": "3",
-    "몸무게": "3",
-}, {
-    "이름": "씨-독",
-    "나이": "72년생",
-    "점프력": "2",
-    "몸무게": "1",
-}, {
-    "이름": "코볼독",
-    "나이": "59년생",
-    "점프력": "1",
-    "몸무게": "1",
-},
-]
-for dog in dogs:
-    for num in range(int(dog['점프력']) - 1, len(stone), int(dog['점프력'])):
-        dog['die'] = False
-        stone[num] -= int(dog['몸무게'])
-        if stone[num] < 0:
-            dog['die'] = True
-            break
-
-for dog in dogs:
-    if dog['die'] == False:
-        print(dog['이름'])
---
+++ weniv/tree.py
@@ -0,0 +1,77 @@
+''' 문제
+1. 이 간선들을 2진 깊이우선 탐색하며 작은 값만을 선택해서, 또는 큰 값만을 선택해서 내려와야 합니다.
+2. 아래 결과값을 단서로 삼아 다음 미션지로 향하세요! 단, 코드로 풀어야 합니다.
+'''
+'''
+DFS 스택을 이용
+조건 : 스택에 담았던 노드는 더 이상 스택에 담지 않는다.
+조건2: 방문했던 노드도 더 이상 스택에 담지 않는다.
+
+START : 첫 노드를 스택에 쌓는다.
+while stack:
+    1. 스택에서 노드를 꺼낸다. 방문 안했다면 방문했다는 표시를 한다.
+    2. 자식 중 스택에 없는 노드를 전부 스택에 넣는다.
+'''
+graph = {100: set([67, 66]),
+         67: set([100, 82, 63]),
+         66: set([100, 73, 69]),
+         82: set([67, 61, 79]),
+         63: set([67]),
+         73: set([66]),
+         69: set([66, 65, 81]),
+         61: set([82]),
+         79: set([82, 87, 77]),
+         65: set([69, 84, 99]),
+         81: set([69]),
+         87: set([79, 31, 78]),
+         77: set([79]),
+         84: set([65]),
+         99: set([65]),
+         31: set([87]),
+         78: set([87])}
+
+
+def only_max_dfs(first_node):
+    # START
+    stack = [first_node]
+    visit = []
+
+    # 순회
+    while stack:
+        node = stack.pop()
+        if node not in visit:
+            visit.append(node)
+            nums = list(graph[node] - set(stack) - set(visit))
+            if not nums:
+                break
+            stack.append(max(nums))
+            print(f'visit : {visit}')
+            print(f'stack : {stack}')
+    return visit
+
+
+def only_min_dfs(first_node):
+    # START
+    stack = [first_node]
+    visit = []
+
+    # 순회
+    while stack:
+        node = stack.pop()
+        if node not in visit:
+            visit.append(node)
+            nums = list(graph[node] - set(stack) - set(visit))
+            if not nums:
+                break
+            stack.append(min(nums))
+            print(f'visit : {visit}')
+            print(f'stack : {stack}')
+    return visit
+
+
+max_visits = only_max_dfs(100)
+min_visits = only_min_dfs(100)
+print(max_visits, min_visits)
+
+print(''.join(list(map(chr, max_visits))))
+print(''.join(list(map(chr, min_visits))))
